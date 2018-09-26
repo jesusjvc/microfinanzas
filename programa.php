daa19520efccfc1010c21a3fc1de27be5defc5e4
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-09-26 13:12:09 -0500

    programa

--- programa.php
+++ programa.php
@@ -39,112 +39,154 @@
                 
                 <article>
                   
-                  <table>
-                      <tr>
-                          <td>Próximamente</td>
-                      </tr>
-                  </table>
-                   <!--
-                    <table>
+                  
+                   
+                    <table class="programa" border="1">
+                       <thead>
                         <tr>
-                            <td>08:00 - 09:00</td>
-                            <td>Registro de Participantes</td>
+                          <th width="20%" style="text-align: center; vertical-align: middle;">Horario</th>
+                          <th width="30%" style="text-align: center; vertical-align: middle;">Tema</th>
+                          <th width="50%" style="text-align: center; vertical-align: middle;">Ponentes</th>
+                        </tr>
+                      </thead>
+                        <tr>
+                            <td style="text-align: center; vertical-align: middle;">9:30 - 10:45</td>
+                            <td style="text-align: left; vertical-align: middle;">Inauguración <br>(Salón Olmeca 4)</td>
+                            <td style="text-align: left; vertical-align: middle;">Ildefonso Guajardo Villarreal, Secretario de Economía<br>
+Cynthia Villarreal Muraira, Coordinadora General del Programa Nacional de Financiamiento al Microempresario y a la Mujer Rural (PRONAFIM) <br>
+Bernardo González Rosas, Presidente de la Comisión Nacional Bancaria y de Valores (CNBV)<br>
+Mario Di Costanzo Armenta, Presidente de la Comisión Nacional para la Protección y Defensa de los Usuarios de Servicios Financieros (CONDUSEF)<br>
+Narcedalia Ramirez Pineda,  Directora General del Instituto Nacional de la Economía Social (INAES)<br>
+Enrique Martínez y Morales,  Director General de la Financiera Nacional de Desarrollo Agropecuario, Rural, Forestal y Pesquero (FND)<br>
+Virgilio Andrade Martínez, Director General del Banco del Ahorro Nacional y Servicios Financieros (BANSEFI)<br>
+Diego Filiberto Duque Robledo, Presidente del Consejo Directivo de ProDesarrollo
+
+</td>
                         </tr>
                         <tr>
-                            <td>09:00</td>
-                            <td><strong>INAUGURACIÓN</strong>
-                                <p>Con la participacion del Secretario de Economía, C. Idelfonso Guajardo Villareal</p>
-                            </td>
+                            <td style="text-align: center; vertical-align: middle;">10:45 -12:15</td>
+                            <td style="text-align: left; vertical-align: middle;">Plenaria Inaugural<br>
+A cuatro décadas de su creación, una perspectiva global y nacional de las microfinanzas <br>
+(Salón Olmeca 4)
+</td>
+                            <td style="text-align: left; vertical-align: middle;">Elisabeth Rhyne, Directora Gerente del Centro para la Inclusión Financiera, Acción <br>
+Leo Abruzzese, Director Global de Política Pública, The Economist Intelligence Unit<br>
+Georgina Vázquez, Gerente de Programa, OMTRIX <br>
+Fernando Sandoval Oseguera, Director de Instituciones Financieras, HR Ratings <br>
+Moderadora: Lizbeth Fajury, Asociada, Marulanda & Consultores
+
+</td>
                         </tr>
                         <tr>
-                            <td>09:30 - 11:00 <span class="moderador">MODERADOR</span></td>
-                            <td><strong>PLENARIA 1:</strong> LOREM SUAM FACERE OCASSIONEM ARRIPERE, TUA CAUTIO NOSTRA
-                                <p>Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> moderador Lorem Suam Facere Ocassionem / Cargo Empresare Corporat</p>
-                            </td>
+                            <td style="text-align: center; vertical-align: middle;">10:45 -12:15</td>
+                            <td></td>
+                            <td style="text-align: left; vertical-align: middle;" >Receso / Networking (Salón Olmeca 2)
+</td>
+                           
+                        </tr>
+                        
+                        <tr>
+                            <td style="text-align: center; vertical-align: middle;">12:30 -13:45</td>
+                            <td style="text-align: left; vertical-align: middle;">Simultánea IA<br>
+Innovando en productos microfinancieros<br>
+(Salón Olmeca 4)
+</td>
+                            <td style="text-align: left; vertical-align: middle;">Ryan Newton, Gerente en Asesoramiento Estratégico, Women’s World Banking<br>
+Lucía Pérez, Gerente de Operaciones, VisionFund México<br>
+Fernando Flores Mendoza, Representante Legal, Credicrece<br>
+Moderador: Isaías Naim López, Director General Adjunto de Planeación y Política Pública,  PRONAFIM<br>
+
+
+</td>
+                        </tr>
+                        
+                        <tr>
+                            <td style="text-align: center; vertical-align: middle;">12:30 –13:45</td>
+                            <td style="text-align: left; vertical-align: middle;">Simultánea IB<br>
+¿Qué está pasando con las tasas de interés?<br>
+(Salón Olmeca 1)
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">Marco Antonio López Pérez, Vicepresidente de Supervisión de Grupos e Intermediarios Financieros, CNBV<br>
+Pablo Cotler Ávalos, Académico del Departamento de Economía, Universidad Iberoamericana <br>
+Juan Francisco Fernández, Presidente del Consejo de Administración, Crediclub<br>
+Moderador: Jorge Mario Soto Romero, Titular del Órgano Interno de Control, Secretaría de Economía
+
+</td>
+                        </tr>
+                        
+                        <tr>
+                            <td style="text-align: center; vertical-align: middle;">13:45 – 15:15</td>
+                            <td style="text-align: left; vertical-align: middle;">
+</td>
+                            <td style="text-align: left; vertical-align: middle;">COMIDA (Salón Mexica 1)
+</td>
+                        </tr>
+                         <tr>
+                            <td style="text-align: center; vertical-align: middle;">15:15 -16:30</td>
+                            <td style="text-align: left; vertical-align: middle;">Simultánea IIA<br>
+Rompiendo paradigmas en la educación financiera y empresarial <br>
+(Salón Olmeca 4)
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">
+Juan Navarrete, Director de Negocios y Desarrollo de Alianzas, Fundación Capital <br>
+María Matilde Olazábal Forcén, Directora, Freedom From Hunger México<br>
+Juan Vega González, Gerente de Proyecto para América Latina, EduFinance: Opportunity International <br>
+Moderador:  Leonardo Jorge Torres Góngora, Director General de Educación Financiera, CONDUSEF 
+
+
+</td>
                         </tr>
                         <tr>
-                            <td>11:00 - 11:15</td>
-                            <td>Coffe Break</td>
+                            <td style="text-align: center; vertical-align: middle;">16:30 – 16:45</td>
+                            <td style="text-align: left; vertical-align: middle;">
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">
+Receso / Networking (Salón Olmeca 2)
+
+
+</td>
+                        </tr>
+                         <tr>
+                            <td style="text-align: center; vertical-align: middle;">16:45-17:45</td>
+                            <td style="text-align: left; vertical-align: middle;">Conferencia Magistral <br>
+Evolución y desafíos en las microfinanzas <br>
+(Salón Olmeca 4)
+
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">
+Timothy Ogden, Director General del Financial Access Initiative, New York University 
+
+
+</td>
                         </tr>
                         <tr>
-                            <td>11:15 - 12:45  <span class="moderador">MODERADOR</span></td>
-                            <td><strong>SESIÓN SIMULTÁNEA 1:</strong> LOREM SUAM FACERE OCASSIONEM ARRIPERE, TUA CAUTIO NOSTRA
-                                <p>Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> moderador Lorem Suam Facere Ocassionem / Cargo Empresare Corporat</p>
-                            </td>
+                            <td style="text-align: center; vertical-align: middle;">17:45 –18:00</td>
+                            <td style="text-align: left; vertical-align: middle;">
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">
+Clausura (Salón Olmeca 4)
+
+
+</td>
                         </tr>
                         <tr>
-                            <td><span class="moderador">MODERADOR</span></td>
-                            <td><strong>SESIÓN SIMULTÁNEA 2:</strong> LOREM SUAM FACERE OCASSIONEM ARRIPERE, TUA CAUTIO NOSTRA
-                                <p>Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> moderador Lorem Suam Facere Ocassionem / Cargo Empresare Corporat</p>
-                            </td>
+                            <td style="text-align: center; vertical-align: middle;">18:00</td>
+                            <td style="text-align: left; vertical-align: middle;">
+
+</td>
+                            <td style="text-align: left; vertical-align: middle;">
+CÓCTEL(Salón Mexica 1)
+
+
+</td>
                         </tr>
-                        <tr>
-                            <td>12:45 - 13:45  <span class="moderador">MODERADOR</span></td>
-                            <td><strong>PLENARIA 2:</strong> LOREM SUAM FACERE OCASSIONEM ARRIPERE, TUA CAUTIO NOSTRA
-                                <p>Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> Lorem Suam Facere Ocassionem / Cargo Empresare Corporat <br> moderador Lorem Suam Facere Ocassionem / Cargo Empresare Corporat</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table> -->
+                       
+                    </table> 
                 </article>
             </section>
         </div>
