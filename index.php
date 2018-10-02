87c80c2552a65db1e1ebfbee460dafc9811becc3
Jesus <jesus@esbrillante.mx>
2018-10-02 18:54:46 -0500

    nuevos ponentes

--- index.php
+++ index.php
@@ -278,6 +278,32 @@
                                 <p>Académico del Departamento de Economía, Universidad Iberoamericana</p>
                             </div>
                         </div>
+                        
+                        <div class="item">
+                            <img src="img/ponentes/Juan-Hernandez.jpg" alt="">
+                            <div class="info">
+                                <h3>Juan Antonio Hernández Martínez</h3>
+                                <p>Director del Programa de Seguridad Alimentaria para Zonas Rurales de la FAO en el Estado de México</p>
+                            </div>
+                        </div>
+                        
+                        <div class="item">
+                            <img src="img/ponentes/Lucia-Perez.jpg" alt="">
+                            <div class="info">
+                                <h3>Lucia Pérez Torres</h3>
+                                <p>Directora de Operaciones de Vision Fund México</p>
+                            </div>
+                        </div>
+                        
+                           <div class="item">
+                            <img src="img/ponentes/MariaMatilde_FFH.jpg" alt="">
+                            <div class="info">
+                                <h3>María Matilde Olazábal Forcén</h3>
+                                <p>Directora, Freedom From Hunger México</p>
+                            </div>
+                        </div>
+                        
+                        
                     </div>
                 </article>
             </section>
@@ -461,7 +487,7 @@
                         <div class="item"><img src="img/expomf/patrocinadores/Logos-16.svg" alt=""></div>
                         <div class="item"><img src="img/expomf/patrocinadores/Logos-17.svg" alt=""></div>
                         <div class="item"><img src="img/expomf/patrocinadores/Logos-18.svg" alt=""></div>
-
+                        <div class="item"><img src="img/expomf/patrocinadores/Logos-19.svg" alt=""></div>
 
 
 
