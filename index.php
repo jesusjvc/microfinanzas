4022f5346f0120614b0a881a54721869c9c77829
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-10-03 11:58:17 -0500

    cambios_index con agenda

--- index.php
+++ index.php
@@ -307,9 +307,189 @@
                         
                     </div>
                 </article>
+                
             </section>
         </div>
+ <div id="programa" class="container template-section">
+            
+            <section id="intro">
 
+             
+                
+                <article>
+                  
+                  <table class="programa" width="0">
+<tbody>
+<tr class="blanco">
+<td width="123" style="text-align: center; vertical-align: middle;"><strong>Horario</strong></td>
+<td width="161" style="text-align: center; vertical-align: middle;"><strong>Tema</strong></td>
+<td width="586" style="text-align: center; vertical-align: middle;"><strong>Ponentes</strong></td>
+</tr>
+<tr>
+<td width="123" ><strong>08:30 a 09:30</strong></td>
+    <td colspan="2" width="747" style="text-align: center; vertical-align: middle;"><strong>REGISTRO DE PARTICIPANTES</strong> <small>(Accesos Salones Olmeca)</small> </td>
+</tr>
+<tr class="magistral">
+<td width="123" ><strong>9:30 - 10:45</strong></td>
+<td width="161"><strong>Inauguración </strong><br>
+
+<small>(Salón Olmeca 4)</small>
+
+&nbsp;</td>
+<td width="586" style="vertical-align: middle;"><strong>Presidida por el Secretario de Economía, Ildefonso Guajardo Villarreal</strong></td>
+</tr>
+<tr class="primera">
+<td width="123"><strong>10:45 -12:15</strong>
+
+ </td>
+<td width="161"><strong>Plenaria Inaugural</strong><br>
+
+A cuatro décadas de su creación, una perspectiva global y nacional de las microfinanzas<br>
+
+<small>(Salón Olmeca 4)</small></td>
+<td width="586" style="vertical-align: middle;"> 
+
+<strong>• Elisabeth Rhyne</strong>, Directora Gerente del Centro para la Inclusión Financiera, Acción<br>
+
+<strong>• Leo Abruzzese</strong>, Director Global de Política Pública, The Economist Intelligence Unit<br>
+
+<strong>• Georgina Vázquez</strong>, Gerente de Programa, OMTRIX<br>
+
+<strong>• Fernando Sandoval Oseguera</strong>, Director de Instituciones Financieras, HR Ratings<br>
+
+<small>• MODERADORA:</small> <strong>Lizbeth Fajury</strong>, Asociada, Marulanda &amp; Consultores
+
+ </td>
+</tr>
+<tr>
+<td width="123"><strong>12:15 – 12:30</strong></td>
+<td colspan="2" width="747" style="text-align: center; vertical-align: middle;"><strong>RECESO / NETWORKING</strong> <small>(Salón Olmeca 2)</small></td>
+</tr>
+<tr class="simulacion">
+<td width="123"><strong>12:30 -13:45</strong>
+
+ 
+
+ </td>
+<td width="161"><strong>Simultánea IA</strong><br>
+
+Innovando en productos microfinancieros<br>
+
+<small>(Salón Olmeca 4)</small></td>
+<td width="586">
+
+<strong>• Ryan Newton</strong>, Gerente en Asesoramiento Estratégico, Women’s World Banking<br>
+
+<strong>• Lucía Pérez, </strong>Gerente de Operaciones, VisionFund México<br>
+
+<strong>• Juan Antonio Hernández Martínez</strong>, Representante Legal, Credicrece<br>
+
+    <small>MODERADOR:</small> <strong>Isaías Naim López</strong>, Director General Adjunto de Planeación y Política Pública,  PRONAFIM
+
+&nbsp;</td>
+</tr>
+<tr class="simulacion">
+<td width="123"><strong>12:30 –13:45</strong></td>
+<td width="161"><strong>Simultánea IB</strong><br>
+
+¿Qué está pasando con las tasas de interés?<br>
+
+<small>(Salón Olmeca 1)</small>
+
+&nbsp;</td>
+<td width="586"><strong>• Marco Antonio López Pérez,</strong> Vicepresidente de Supervisión de Grupos e Intermediarios Financieros, CNBV<br>
+
+<strong>• Pablo Cotler Ávalos</strong>, Académico del Departamento de Economía, Universidad Iberoamericana<br>
+
+<strong>• Juan Francisco Fernández</strong>, Presidente del Consejo de Administración, Crediclub<br>
+
+    <small>MODERADOR:</small> <strong>Jorge Mario Soto Romero</strong>, Titular del Órgano Interno de Control, Secretaría de Economía
+
+&nbsp;</td>
+</tr>
+<tr>
+<td width="123"><strong>13:45 – 15:15</strong></td>
+<td colspan="2" width="747" style="text-align: center; vertical-align: middle;"><strong>COMIDA</strong> <small>(Salón Mexica 1)</small></td>
+</tr>
+<tr class="simulacion">
+<td width="123"><strong>15:15 -16:30</strong></td>
+<td width="161"><strong>Simultánea IIA</strong><br>
+
+Rompiendo paradigmas en la educación financiera y empresarial<br>
+
+<small>(Salón Olmeca 4)</small></td>
+<td width="586"> 
+
+<strong>• Juan Navarrete, </strong>Director de Negocios y Desarrollo de Alianzas, Fundación Capital<br>
+
+<strong>• María Matilde Olazábal Forcén</strong>, Directora, Freedom From Hunger México<br>
+
+<strong>• Juan Vega González</strong>, Gerente de Proyecto para América Latina, EduFinance: Opportunity International<br>
+<small>MODERADOR:</small> <strong>Leonardo Jorge Torres Góngora</strong>, Director General de Educación Financiera, CONDUSEF
+
+&nbsp;</td>
+</tr>
+<tr class="simulacion">
+<td width="123"><strong>15:15-16:30</strong>
+
+ </td>
+<td width="161"><strong>Simultánea IIB</strong><br>
+
+Nuevas tecnologías: ideas que impulsan las microfinanzas<br>
+
+<small>(Salón Olmeca 1)</small></td>
+<td width="586"> 
+
+<strong>• René Romero Arrechavala, </strong>Asesor Senior de Centro América y el Caribe, Amarante Consulting<br>
+
+<strong>• Alonso Castellot, </strong> Director, Intelistregia<br>
+
+<strong>• Gerd Hans Weißbach, </strong>Director para América Latina y el Caribe, Sparkassenstiftung für Internationale Kooperation<br>
+
+    <small>MODERADOR:</small> <strong>Gustavo Salaiz  Gabriel</strong>, Director General Adjunto de Explotación de Información, CNBV
+
+&nbsp;</td>
+</tr>
+<tr>
+<td width="123"><strong>16:30 – 16:45</strong></td>
+<td colspan="2" width="747" style="text-align: center; vertical-align: middle;"><strong>RECESO / NETWORKING</strong> <small>(Salón Olmeca 2)</small>
+
+&nbsp;</td>
+</tr>
+<tr class="magistral">
+<td width="123"><strong>16:45-17:45</strong>
+
+ </td>
+<td width="161"><strong>Conferencia Magistral</strong><br>
+
+Evolución y desafíos en las microfinanzas<br>
+
+<small>(Salón Olmeca 4)</small></td>
+<td width="586" style="vertical-align: middle;"><strong>• Timothy Ogden,</strong> Director General del Financial Access Initiative
+</td>
+</tr>
+<tr class="clausura">
+<td width="123"><strong>17:45 –18:00</strong>
+
+ </td>
+<td colspan="2" width="747" style="text-align: center; vertical-align: middle;"><strong>CLAUSURA</strong> 
+
+&nbsp;</td>
+</tr>
+<tr class="coctel">
+<td width="123"><strong>18:00</strong></td>
+<td colspan="2" width="747" style="text-align: center; vertical-align: middle;">
+
+    <strong>CÓCTEL </strong>
+
+&nbsp;</td>
+</tr>
+</tbody>
+</table>
+                   
+                </article>
+            </section>
+        </div>
         <div id="hospedaje" class="container template-section">
             <section id="intro">
 
@@ -437,6 +617,7 @@
                 </article>
             </section>
         </div>
+        
 
         <div id="expomf" class="container template-section">
 
